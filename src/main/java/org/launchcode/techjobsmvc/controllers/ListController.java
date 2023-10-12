ff3584087587b91a558c42dc50e6cc54adb6015e
sharee-tech <mail.sharee@gmail.com>
2023-10-12 11:05:19 -0500

    Completed Task #2

--- src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
@@ -46,25 +46,26 @@ public class ListController {
         return "list";
     }
 
-    @GetMapping(value = "jobs")
-    public String listJobs(Model model) {
-        ArrayList<Job> jobs;
-        jobs = JobData.findAll();
-        model.addAttribute("jobs", jobs);
-        return "list-jobs";
-    }
-//    public String listJobsByColumnAndValue(Model model, @RequestParam String column, @RequestParam(required = false) String value) {
+//    @GetMapping(value = "jobs")
+//    public String listJobs(Model model) {
 //        ArrayList<Job> jobs;
-//        if (column.equals("all")){
-//            jobs = JobData.findAll();
-//            model.addAttribute("title", "All Jobs");
-//        } else {
-//            jobs = JobData.findByColumnAndValue(column, value);
-//            model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
-//        }
+//        jobs = JobData.findAll();
 //        model.addAttribute("jobs", jobs);
-//
 //        return "list-jobs";
 //    }
+    @GetMapping(value = "jobs")
+    public String listJobsByColumnAndValue(Model model, @RequestParam String column, @RequestParam(required = false) String value) {
+        ArrayList<Job> jobs;
+        if (column.equals("all")){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(column, value);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
+        }
+        model.addAttribute("jobs", jobs);
+
+        return "list-jobs";
+    }
 }
 
